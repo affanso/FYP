d19d28b9428e54f5138d1ba3ab2ecae65304592c
affanso <affansohail78@gmail.com>
2025-06-26 21:01:15 +0500

    --

--- app.py
+++ app.py
@@ -318,5 +318,6 @@ def check_session():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))  # fallback port if PORT isn't set
     app.run(host='0.0.0.0', port=port)
+
