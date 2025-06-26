a375b9ef084d1214002fb7e986249cf1030a83aa
affanso <affansohail78@gmail.com>
2025-06-26 22:09:41 +0500

    --

--- models.py
+++ models.py
@@ -10,7 +10,14 @@ from datetime import datetime
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class=Base)
+#db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy(
+    model_class=Base,
+    engine_options={
+        "pool_pre_ping": True,
+        "pool_recycle": 280
+    }
+)
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
