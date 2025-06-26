c590ef75760e18206ec8a13845e9aca322f0e86a
affanso <affansohail78@gmail.com>
2025-06-26 19:33:03 +0500

    change port and host

--- app.py
+++ app.py
@@ -317,5 +317,8 @@ def check_session():
     return jsonify({"active": session.get('active', False)})
 
 
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=True)
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
